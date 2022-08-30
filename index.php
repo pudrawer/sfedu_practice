ad38ef1710993a66b36320720da97c24f0cd8e59
Butenko Aleksandr <abutenko@lachestry.com>
2022-08-30 18:09:29 +0300

    Fix bug into home page
    [#601]

--- index.php
+++ index.php
@@ -6,4 +6,4 @@ require_once './vendor/autoload.php';
 
 $router = new Router();
 $controller = $router->chooseController($_SERVER['REQUEST_URI'] ?? '');
-$controller->listAction();
+$controller ?? $controller->listAction();
